5361dcf3a49b1d747db7a87c6734a1a58e1cea7e
bobbywilliamskgh <bobbywilliam86@gmail.com>
2022-06-17 22:33:22 +0700

    delete nonmesin test

--- test/tools_bloc_test.dart
+++ test/tools_bloc_test.dart
@@ -47,5 +47,69 @@ void main() {
                     estimasiWaktu: '14 hari'),
               ])
             ]);
+    blocTest("Menghapus Alat yang Bukan Mesin",
+        build: () => ToolsBloc(),
+        act: (ToolsBloc bloc) async {
+          bloc.add(
+            LoadTools(tools: [
+              ToolsModel(
+                  nama: 'alat1',
+                  id: '76187230',
+                  jenisAlat: 'Tidak Mesin',
+                  merk: 'Honda2',
+                  harga: '45.000',
+                  dimensi: '100',
+                  berat: '0.4',
+                  sumberDaya: 'biodiesel',
+                  kapasitas: '3 ton',
+                  kondisi: 'Baik',
+                  deskripsiKerusakan: 'Tidak ada baut',
+                  tempatPerbaikan: '-',
+                  rencana: '-',
+                  estimasiWaktu: '-'),
+            ]),
+          );
+          bloc.add(
+            DeleteTool(
+              tool: ToolsModel(
+                  nama: 'alat1',
+                  id: '76187230',
+                  jenisAlat: 'Tidak Mesin',
+                  merk: 'Honda2',
+                  harga: '45.000',
+                  dimensi: '100',
+                  berat: '0.4',
+                  sumberDaya: 'biodiesel',
+                  kapasitas: '3 ton',
+                  kondisi: 'Baik',
+                  deskripsiKerusakan: 'Tidak ada baut',
+                  tempatPerbaikan: '-',
+                  rencana: '-',
+                  estimasiWaktu: '-'),
+            ),
+          );
+        },
+        expect: () => [
+              // ToolsInitital(),
+              // ToolsLoading(),
+              ToolsLoaded(tools: [
+                ToolsModel(
+                    nama: 'alat1',
+                    id: '76187230',
+                    jenisAlat: 'Tidak Mesin',
+                    merk: 'Honda2',
+                    harga: '45.000',
+                    dimensi: '100',
+                    berat: '0.4',
+                    sumberDaya: 'biodiesel',
+                    kapasitas: '3 ton',
+                    kondisi: 'Baik',
+                    deskripsiKerusakan: 'Tidak ada baut',
+                    tempatPerbaikan: '-',
+                    rencana: '-',
+                    estimasiWaktu: '-'),
+              ]),
+              ToolsLoaded(tools: [])
+            ]);
   });
 }
