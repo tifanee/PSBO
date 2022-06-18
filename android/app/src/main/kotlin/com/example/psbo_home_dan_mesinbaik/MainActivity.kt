70c7b49335c6773a3253359d8e28eac70ebbc4ce
bobbywilliamskgh <bobbywilliam86@gmail.com>
2022-06-18 09:24:03 +0700

    delte files in ex-main branch

--- android/app/src/main/kotlin/com/example/psbo_home_dan_mesinbaik/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.psbo_home_dan_mesinbaik
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/condition.dart
+++
@@ -1,1 +0,0 @@
-enum Condition { Baik, Rusak }
